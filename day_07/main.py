6d7a5189de45620edaf055c8e78aeb66c7009838
blickdai3690 <blickdai3690@gmail.com>
2025-04-14 20:46:23 +0200

    Added step 2 day07

--- day_07/main.py
+++ day_07/main.py
@@ -4,10 +4,17 @@ word_list = ["aardvark", "baboon", "camel"]
 choice = random.choice(word_list)
 print(choice)
 
+placeholder = ''
+for c in choice:
+    placeholder += '_'
+print(placeholder)
+
 guess = input("Guess a letter ").lower()
+display = ''
 
 for letter in choice:
     if letter == guess:
-        print("Right")
+        display += letter
     else:
-        print("Wrong")
+        display += '_'
+print(display)
